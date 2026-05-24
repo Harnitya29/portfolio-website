562990ee5ffd4ca049ba1dc9af973fd117c15754
Harnitya29 <142140691+Harnitya29@users.noreply.github.com>
2026-05-24 22:10:49 +0530

    Update PhotosClient.tsx

--- src/app/photos/PhotosClient.tsx
+++ src/app/photos/PhotosClient.tsx
@@ -459,7 +459,9 @@ export default function PhotosClient() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
               </svg>
             </motion.div>
-            <p className="text-zinc-400 text-center"><p>まだ何もありません</p></p>
+            <p className="text-zinc-400 text-center"><p className="text-zinc-400 text-center">
+  記録はまだ存在しません
+</p>
           </motion.div>
         )}
       </div>
